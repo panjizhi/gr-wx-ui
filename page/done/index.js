a487bf510963016de8d53736bc237af94ed4f6a3
fayipan <fayipan@lagou.com>
2017-06-19 11:43:27 +0800

    update

---
+++ page/done/index.js
@@ -0,0 +1,29 @@
+const config = require('../../config');
+
+const app = getApp();
+
+Page({
+    data: {
+        user: null,
+        list: []
+    },
+    onLoad(options) {
+        console.log(options.id);
+
+        app.getUserInfo( user => {
+            this.setData({
+                user
+            });
+        });
+
+        wx.request({
+            url: `${config.requestUrl}/exam`,
+            dataType: 'json',
+            success: res => {
+                this.setData({
+                    list: res.data.collections
+                });
+            }
+        })
+    }
+});
