40c767b139c7be2c6422a0fc29ff463533a061b1
fayipan <fayipan@lagou.com>
2017-06-26 20:43:24 +0800

    update

---
+++ page/review/index.js
@@ -0,0 +1,62 @@
+const config = require('../../config');
+
+const app = getApp();
+
+const typesMap = {
+    'choice': '单选题',
+    'multiple-choices': '多选题',
+    'cloze': '填空题'
+};
+
+Page({
+    data: {
+        paper: null,
+        currQuestion: null,
+        currIndex: 0,
+        user: null,
+        typesMap
+    },
+    onLoad(options) {
+        const self = this;
+
+        app.getUserInfo( user => {
+            self.setData({
+                user
+            });
+
+            wx.request({
+                url: `${config.requestUrl}/exam/detail/${options.id}`,
+                dataType: 'json',
+                success: res => {
+                    let data = res.data || {};
+                    let questions = data.questions || [];
+
+                    questions.forEach( item => {
+                        item.options = (item.options || []).map( option => {
+                            return {
+                                value: option.charAt(0).toUpperCase(),
+                                name: option,
+                                checked: false
+                            };
+                        });
+                    });
+
+                    self.setData({
+                        paper: data,
+                        currQuestion: questions[self.data.currIndex]
+                    });
+                }
+            });
+        });
+    },
+    nextQuestion() {
+        const selfData = this.data;
+        const currIndex = selfData.currIndex + 1;
+        const currQuestion = selfData.paper.questions[currIndex];
+
+        this.setData({
+            currIndex,
+            currQuestion
+        });
+    }
+});
